949332a52472228522942371853d109a13fb407a
robin <46913448+robinlilychu@users.noreply.github.com>
2021-09-27 13:25:20 -0700

    [Lab04] Adding starter code

---
+++ lab04/accumulatortests.s
@@ -0,0 +1,46 @@
+.import lotsofaccumulators.s
+
+.data
+inputarray: .word 1,2,3,4,5,6,7,0
+
+TestPassed: .asciiz "Test Passed!"
+TestFailed: .asciiz "Test Failed!"
+
+.text
+# Tests if the given implementation of accumulate is correct.
+#Input: a0 contains a pointer to the version of accumulate in question. See lotsofaccumulators.s for more details
+#
+#
+#
+#The main function currently runs a simple test that checks if accumulator works on the given input array. All versions of accumulate should pass this.
+#Modify the test so that you can catch the bugs in four of the five solutions!
+main:
+    la a0 inputarray
+    jal accumulatorone
+    li t0 28
+    beq a0 t0 Pass
+Fail:
+    la a0 TestFailed
+    jal print_string
+    j End
+Pass:
+    la a0 TestPassed
+    jal print_string
+End:
+    jal exit
+
+print_int:
+	mv a1 a0
+    li a0 1
+    ecall
+    jr ra
+    
+print_string:
+	mv a1 a0
+    li a0 4
+    ecall
+    jr ra
+    
+exit:
+    li a0 10
+    ecall
