d369dde8c8d943679be16789874f20856fe38490
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2021-10-20 13:52:37 -0700

    testing

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -13,14 +13,14 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
   for (int i = 0; i < n; i += blocksize) {
-      for (int j = 0; j < n; j += blocksize) {
-          for (int x = i; x < i + blocksize; x++) {
-              for (int y = j; y < j +  blocksize; y++) {
-                 if (x < n && y < n) {
-                   dst[y + x * n] = src[x + y * n];
-                  }
-              }
-          }
-      }
-  }
-} 
+        for (int j = 0; j < n; j += blocksize) {
+            for (int x = i; x < i + blocksize; x++) {
+                for (int y = j; y < j +  blocksize; y++) {
+	                  if (x < n && y < n) {
+                    	dst[y + x * n] = src[x + y * n];
+	                   }
+                }
+            }
+        }
+    }
+}
