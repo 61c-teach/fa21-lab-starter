e39fa22b37e9b65f7d312091124d0c90e4c2f89f
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2021-10-21 13:57:45 -0700

    [Lab07] Adding starter

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -11,16 +11,6 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
-void transpose_blocking(int n, int blocksize, int *dst, int *src) {
-  for (int i = 0; i < n; i += blocksize) {
-        for (int j = 0; j < n; j += blocksize) {
-            for (int x = i; x < i + blocksize; x++) {
-                for (int y = j; y < j +  blocksize; y++) {
-	                  if (x < n && y < n) {
-                    	dst[y + x * n] = src[x + y * n];
-	                   }
-                }
-            }
-        }
-    }
-}
+ void transpose_blocking(int n, int blocksize, int *dst, int *src) {
+     // YOUR CODE HERE
+ }
